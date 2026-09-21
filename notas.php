9b4b84ecd7c609c4f61f0c3ab8bb3d62f2cde66f
GuilhermeBatiston <joaoguilhermepradolopes@gmail.com>
2026-09-21 16:07:06 -0300

    correção de sintaxe

--- notas.php
+++ notas.php
@@ -12,16 +12,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nota3 = $_POST["nota3"];
     $nota4 = $_POST["nota4"];
     $nota5 = $_POST["nota5"];
-    $media = $_POST["média"];
+   
 
-    $media = (($nota1*2) + ($nota2*3) + ($nota3*1) + ($nota4*1) + ($nota5*3));
+    $media = (($nota1*2) + ($nota2*3) + ($nota3*1) + ($nota4*1) + ($nota5*3))/10;
 
     
 
     if ($media >= 7) {
         $resultado = "Você está aprovado";
-    } elseif ($media >=5 & $media <7) {
+    } 
+    elseif ($media >=5 && $media <7) {
         $resultado = "Você está de recuperação";
+    }
+    else {
+        $resultado= "Você está de reprovado";
     }
 }
 ?>
@@ -40,11 +44,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <form method="POST">
         <input type="text" name="nome" placeholder="Digite seu nome">
         <input type="number" name="idade" placeholder="Digite sua idade"><br><br>
-        <input type="number" name="Nota 1" placeholder="Digite a primeira nota"><br><br>
-        <input type="number" name="Nota 2" placeholder="Digite a segunda nota"><br><br>
-        <input type="number" name="Nota 3" placeholder="Digite a terceira nota"><br><br>
-        <input type="number" name="Nota 4" placeholder="Digite a quarta nota"><br><br>
-        <input type="number" name="Nota 5" placeholder="Digite a quintaa nota"><br><br>
+        <input type="number" name="nota1" placeholder="Digite a primeira nota"><br><br>
+        <input type="number" name="nota2" placeholder="Digite a segunda nota"><br><br>
+        <input type="number" name="nota3" placeholder="Digite a terceira nota"><br><br>
+        <input type="number" name="nota4" placeholder="Digite a quarta nota"><br><br>
+        <input type="number" name="nota5" placeholder="Digite a quintaa nota"><br><br>
 
         <button type="submit">Enviar</button>
     </form>
